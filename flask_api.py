53dcce6b581e39b8efaf66fec4de068844d7b6c1
MrShiva <57280845+shivanagendrakakumanu@users.noreply.github.com>
2022-06-10 00:19:29 +0530

    Create flask_api.py

---
+++ flask_api.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restful import Resource, Api
+import time
+
+app = Flask(__name__)
+api = Api(app)
+
+class GetDetail(Resource):
+    def get(self):
+        # time.sleep(6)
+        return "Python API"
+
+api.add_resource(GetDetail, '/getname')
+
+if __name__ == '__main__':
+    app.run(debug=True)
