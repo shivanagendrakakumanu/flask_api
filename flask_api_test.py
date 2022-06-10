687e8ae949f8b31915077877dca58d3585b70365
MrShiva <57280845+shivanagendrakakumanu@users.noreply.github.com>
2022-06-10 14:53:31 +0530

    Update flask_api_test.py

--- flask_api_test.py
+++ flask_api_test.py
@@ -1,9 +1,12 @@
-import requests
 
-def test_post_headers_body_json():
-    url = 'http://127.0.0.1:5000/getname'
-    
-    resp = requests.get(url)       
+import requests
+import flask_api 
+
+def test_get_name():
+    web = flask_api.app.test_client()
+    resp = web.get('/getname')
+    # url = 'http://127.0.0.1:5000/getname'
+    # resp = requests.get(url)
     assert resp.status_code == 200
     
     # print response full body as text
